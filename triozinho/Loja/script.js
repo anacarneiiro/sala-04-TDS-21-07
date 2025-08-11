5399b482a097a5f56d8f518b953cede3fa1f7f14
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-11 07:56:35 -0300

    anaacla

--- triozinho/Loja/script.js
+++ triozinho/Loja/script.js
@@ -1,59 +1,61 @@
 let produtos
 
-window.onload = function(){
-    var storeUser = localStorage.getItem("usuario")
-    var user = JSON.parse(storeUser)
+window.onload = function () {
+    var storedUser = localStorage.getItem("usuario");
+    var user = JSON.parse(storedUser);
 
     var dataEntrada = new Date(user.dataEntrada);
-    var dataFormatada = dataEntrada.toDateString("pt-BR", {
-        day:"2-digit",
-        month:"2-digit",
-        year:"nuneric",
-        hour:"2-digit",
-        minute:"2-digit",
-    })
+    var dataFormatada = dataEntrada.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    });
 
-    document.getElementById("perfil").innerText = dataFormatada
-    document.getElementById("user").innerText = user.name
-    document.getElementById("idPerfil").innerText = user.id
+    document.getElementById("perfil").innerText = dataFormatada;
+    document.getElementById("user").innerText = user.name;
+    document.getElementById("idPerfil").innerText = user.id;
+};
 
-}
-
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    //busca dos produtos e armazenamento dos dados na variavel global
     fetch('../Dados/mock.json')
-        .then((response) =>response.json())
-        .then((data) =>{
+        .then((response) => response.json())
+        .then((data) => {
             produtos = data
-            //console.log(data)
-            
+            // console.log(data)
+
             const produtosContainer = document.getElementById('produtos-container')
-            produtos.array.forEach((produto, index) =>{
+            produtos.forEach((produto, index) => {
+
                 const card = document.createElement('div')
                 card.className = 'card'
                 card.style.width = '18rem'
                 card.style.marginRight = '10px'
-                
+
                 const imagem = document.createElement('img')
-                    card.src = produto.imagem
-                    imagem.className = 'card-img-top'
+                imagem.src = produto.imagem
+                imagem.className = 'card-img-top'
 
                 const cardBody = document.createElement('div')
-                  cardBody.className = 'card-body'
+                cardBody.className = 'card-body'
 
                 const cardTitle = document.createElement('h5')
-                  cardTitle.className = 'card-title'
-                  cardTitle.textContent = produto.descricao
+                cardTitle.className = 'card-title'
+                cardTitle.textContent = produto.descricao
 
                 const cardText = document.createElement('p')
-                 cardText.className = 'card-text'
-                 cardText.textContent = 'Preço: $' + produto.preco.toFixed(2)
+                cardText.className = 'card-text'
+                cardText.textContent = 'Preço: $' + produto.preco.toFixed(2)
 
                 const btnAdicionarAoCarrinho = document.createElement('a')
-                  btnAdicionarAoCarrinho.href = '#'
-                  btnAdicionarAoCarrinho.className = 'btn btn-primary btn-adicionar-ao-carrinho'
-                  btnAdicionarAoCarrinho.textContent = "Adicionar ao carrinho"
-                  btnAdicionarAoCarrinho.setAttribute('data-indice'.index)
+                btnAdicionarAoCarrinho.href = '#'
+                btnAdicionarAoCarrinho.className = 'btn btn-primary btn-adicionar-ao-carrinho'
+                btnAdicionarAoCarrinho.textContent = 'Adicionar ao carrinho'
+                btnAdicionarAoCarrinho.setAttribute('data-indice', index)
 
+                //criando os pais e filhos segundo o bootstrap
                 cardBody.appendChild(cardTitle)
                 cardBody.appendChild(cardText)
                 cardBody.appendChild(btnAdicionarAoCarrinho)
@@ -61,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 card.appendChild(imagem)
                 card.appendChild(cardBody)
 
+                produtosContainer.appendChild(card)
             });
-    }).catch((error) => console.error('Erro ao carregar dados', error))
-})
+        }).catch((error) => console.error('Erro ao carregar dados', error))
+});
